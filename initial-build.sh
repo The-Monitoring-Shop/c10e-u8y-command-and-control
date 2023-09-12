5e11b551b9046d7a48010c8b61f63311a94f579c
billfoxTMS <bill.fox@themonitorigshop.com>
2023-09-12 16:46:05 +0100

    Set up Prometheus to remote write to C10e collector

--- initial-build.sh
+++ initial-build.sh
@@ -16,11 +16,12 @@
 WORKING=`dirname $0`
 echo $WORKING
 
-# Step 1: Deploy the application, using helm charts based on the Open Telemetry demo. 
+# Step 1: Deploy the C10e collector
+
+kubectl apply -f $WORKING/c10e_col_conf/chronocollector.yaml
+
+# Step 2: Deploy the application, using helm charts based on the Open Telemetry demo. 
 #         These charts will pull the code from the c10e-u8y-labs-gen github repository.
 
 helm install c10e-u8y-labs-gen $WORKING/helm_charts/lab-gen-initial-build --values $WORKING/lab_gen_values/initial-values.yaml
 
-# Step 2: Deploy the C10e collector
-
-kubectl apply -f $WORKING/c10e_col_conf/chronocollector.yaml
